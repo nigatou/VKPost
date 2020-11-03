7388b2bdcc2f537af789b504a0c19651bfcfec47
nigatou <wholewhy69@gmail.com>
2020-11-04 00:09:56 +0300

    Added Nullable

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -1,26 +1,26 @@
 data class Post(
-        val ownerId: Int,
-        val fromId: Int,
-        val createdBy: Int,
-        var date: Int,
-        val text: String,
-        val replyOwnerId: Int,
-        val replyPostId: Int,
-        val friendsOnly: Boolean,
-        val comments: Array<String>,
-        val copyright: String,
-        val likes: Array<Int>,
-        val reposts: Array<Int>,
-        val views: Array<Int>,
-        val postType: String,
-        val signerId: Int,
-        val canPin: Boolean,
-        val canDelete: Boolean,
-        val canEdit: Boolean,
-        val isPinned: Boolean,
-        val markedAsAds: Boolean,
-        val isFavourite: Boolean,
-        val postponedId: Int,
+        val ownerId: Int?,
+        val fromId: Int?,
+        val createdBy: Int?,
+        var date: Int?,
+        val text: String?,
+        val replyOwnerId: Int?,
+        val replyPostId: Int?,
+        val friendsOnly: Boolean?,
+        val comments: Array<String>?,
+        val copyright: String?,
+        val likes: Array<Int>?,
+        val reposts: Array<Int>?,
+        val views: Array<Int>?,
+        val postType: String?,
+        val signerId: Int?,
+        val canPin: Boolean?,
+        val canDelete: Boolean?,
+        val canEdit: Boolean?,
+        val isPinned: Boolean?,
+        val markedAsAds: Boolean?,
+        val isFavourite: Boolean?,
+        val postponedId: Int?,
         var id: Int = 0,
 ) {
     override fun equals(other: Any?): Boolean {
@@ -57,12 +57,12 @@ data class Post(
 
     override fun hashCode(): Int {
         var result = ownerId
-        result = 31 * result + fromId
-        result = 31 * result + createdBy
-        result = 31 * result + date
+        result = 31 * result!! + fromId!!
+        result = 31 * result + createdBy!!
+        result = 31 * result + date!!
         result = 31 * result + text.hashCode()
-        result = 31 * result + replyOwnerId
-        result = 31 * result + replyPostId
+        result = 31 * result + replyOwnerId!!
+        result = 31 * result + replyPostId!!
         result = 31 * result + friendsOnly.hashCode()
         result = 31 * result + comments.contentHashCode()
         result = 31 * result + copyright.hashCode()
@@ -70,14 +70,14 @@ data class Post(
         result = 31 * result + reposts.contentHashCode()
         result = 31 * result + views.contentHashCode()
         result = 31 * result + postType.hashCode()
-        result = 31 * result + signerId
+        result = 31 * result + signerId!!
         result = 31 * result + canPin.hashCode()
         result = 31 * result + canDelete.hashCode()
         result = 31 * result + canEdit.hashCode()
         result = 31 * result + isPinned.hashCode()
         result = 31 * result + markedAsAds.hashCode()
         result = 31 * result + isFavourite.hashCode()
-        result = 31 * result + postponedId
+        result = 31 * result + postponedId!!
         result = 31 * result + id
         return result
     }
